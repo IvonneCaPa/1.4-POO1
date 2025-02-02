82f96e53790dd810497f2542e0402aba78aa0f84
Ivonne Cantor <ivonnecp@gmail.com>
2025-02-02 16:32:59 +0100

    Ejercicio 01 del primer nivel ok

---
+++ ejercicio01.php
@@ -0,0 +1,24 @@
+<?php
+    class employee{
+        public $nom;
+        public $sou;
+
+        public function initialize(string $nom, int $sou) {
+            $this -> nom = $nom;
+            $this -> sou = $sou;
+        }
+        public function print() : string {
+            if($this -> sou > 6000){
+                $msg = "Debe pagar impuestos";
+            } else {
+                $msg = "NO debe pagar impuestos";
+            }
+
+            return "El empleado $this->nom $msg";
+        }
+    }
+
+    $empleado = new employee();
+    $empleado -> initialize("juan", 1000);
+    echo $empleado -> print() . PHP_EOL;
+?>
